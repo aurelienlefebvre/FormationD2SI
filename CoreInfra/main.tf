f9d53eeafefd4bcded7342112601922c73402e67
Aurelien <aurelien.lefebvre@veolia.com>
2017-11-14 16:23:42 +0100

    Creation route table + route table association

--- CoreInfra/main.tf
+++ CoreInfra/main.tf
@@ -10,8 +10,9 @@ resource "aws_vpc" "mon_vpc" {
 }
 
 resource "aws_subnet" "mon_subnet" {
-  vpc_id     = "${aws_vpc.mon_vpc.id}"
-  cidr_block = "10.0.0.0/16"
+  vpc_id                  = "${aws_vpc.mon_vpc.id}"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 
   tags {
     Name        = "Subnet de Aurelien"
@@ -26,4 +27,23 @@ resource "aws_internet_gateway" "ma_gateway" {
     Name        = "Gateway de Aurelien"
     DateCreated = "14/11/2017"
   }
+}
+
+resource "aws_route_table" "ma_route_table" {
+  vpc_id = "${aws_vpc.mon_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.ma_gateway.id}"
+  }
+
+  tags {
+    Name        = "Route table de Aurelien"
+    DateCreated = "14/11/2017"
+  }
+}
+
+resource "aws_route_table_association" "ma_route_table_association" {
+  subnet_id      = "${aws_subnet.mon_subnet.id}"
+  route_table_id = "${aws_route_table.ma_route_table.id}"
 }
