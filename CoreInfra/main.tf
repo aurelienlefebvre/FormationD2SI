06c55f757844129d909f00e1aeb41d73ea86fa80
Aurelien <aurelien.lefebvre@veolia.com>
2017-11-14 15:55:15 +0100

    Creation subnet

--- CoreInfra/main.tf
+++ CoreInfra/main.tf
@@ -1,1 +1,20 @@
 provider "aws" {}
+
+resource "aws_vpc" "mon_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags {
+    Name        = "VPC de Aurelien"
+    DateCreated = "14/11/2017"
+  }
+}
+
+resource "aws_subnet" "mon_subnet" {
+  vpc_id     = "${aws_vpc.mon_vpc.id}"
+  cidr_block = "10.0.0.0/16"
+
+  tags {
+    Name        = "Subnet de Aurelien"
+    DateCreated = "14/11/2017"
+  }
+}
