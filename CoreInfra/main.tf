ab19591f437297199be0c26b3e1423b75bac1f85
Aurelien <aurelien.lefebvre@veolia.com>
2017-11-15 11:13:54 +0100

    add a second subnet

--- CoreInfra/main.tf
+++ CoreInfra/main.tf
@@ -9,13 +9,18 @@ resource "aws_vpc" "mon_vpc" {
   }
 }
 
-resource "aws_subnet" "mon_subnet" {
-  vpc_id                  = "${aws_vpc.mon_vpc.id}"
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
+variable subnet_cidrs{
+  type="list"
+  default=["10.0.1.0/24","10.0.2.0/24"]
+}
 
+resource "aws_subnet" "mon_subnet" {
+  count = 2
+  vpc_id                  = "${aws_vpc.mon_vpc.id}"
+  cidr_block              = "${element(var.subnet_cidrs,count.index)}"
+  map_public_ip_on_launch = true
   tags {
-    Name        = "Subnet de Aurelien"
+    Name        = "Subnet de Aurelien - ${count.index}"
     DateCreated = "14/11/2017"
   }
 }
@@ -44,6 +49,7 @@ resource "aws_route_table" "ma_route_table" {
 }
 
 resource "aws_route_table_association" "ma_route_table_association" {
-  subnet_id      = "${aws_subnet.mon_subnet.id}"
+  count = 2
+  subnet_id      = "${element(aws_subnet.mon_subnet.*.id,count.index)}"
   route_table_id = "${aws_route_table.ma_route_table.id}"
 }
--- CoreInfra/output.tf
+++ CoreInfra/output.tf
@@ -1,1 +1,15 @@
+output "vpc_id"{
+  value="${aws_vpc.mon_vpc.id}"
+}
 
+output "subnet1_id"{
+  value="${aws_subnet.mon_subnet.0.id}"
+}
+
+output "subnet2_id"{
+  value="${aws_subnet.mon_subnet.1.id}"
+}
+
+output "subnet_ids"{
+  value=["${aws_subnet.mon_subnet.*.id}"]
+}
